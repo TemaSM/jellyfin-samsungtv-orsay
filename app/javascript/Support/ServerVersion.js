27997369330ff0feb5f6821bdc643b5b8ded0b3e
Artem Smirnov <git.tema@smirnov.one>
2026-01-07 18:06:59 +0300

    fix: incorrect version comparison logic (it was always failing)

--- app/javascript/Support/ServerVersion.js
+++ app/javascript/Support/ServerVersion.js
@@ -2,6 +2,26 @@ var ServerVersion = {
 		ServerInfo : null
 }
 
+// Compare semantic versions: returns -1 (a<b), 0 (a==b), 1 (a>b)
+ServerVersion.compareVersions = function(a, b) {
+	var partsA = a.split('.').map(Number);
+	var partsB = b.split('.').map(Number);
+	var len = Math.max(partsA.length, partsB.length);
+
+	for (var i = 0; i < len; i++) {
+		var numA = partsA[i] || 0;
+		var numB = partsB[i] || 0;
+		if (numA > numB) return 1;
+		if (numA < numB) return -1;
+	}
+	return 0;
+};
+
+// Check if version 'current' is at least 'required'
+ServerVersion.isVersionAtLeast = function(current, required) {
+	return this.compareVersions(current, required) >= 0;
+};
+
 ServerVersion.start = function() {
 	document.getElementById("pageContent").innerHTML = "<div class='padding60' style='text-align:center'> \
 		<p style='padding-bottom:5px;'>The Samsung app requires a later version of the Server - Please update it and restart the app</p>";
@@ -17,11 +37,7 @@ ServerVersion.checkServerVersion = function() {
 	var requiredServerVersion = Main.getRequiredServerVersion();
 	var currentServerVersion = this.ServerInfo.Version;
 	
-	if (currentServerVersion >= requiredServerVersion) {
-		return true;
-	} else {
-		return false;
-	}
+	return this.isVersionAtLeast(currentServerVersion, requiredServerVersion);
 }
 
 ServerVersion.keyDown = function() {
